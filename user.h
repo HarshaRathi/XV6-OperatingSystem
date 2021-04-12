00b64147d2d74d2c5b2c6f2e43ae2573e522d596
Harsha Rathi <harsharathi27@gmail.com>
2021-04-12 15:26:10 +0530

    add settimer and setpriority

--- user.h
+++ user.h
@@ -23,6 +23,8 @@ int getpid(void);
 char* sbrk(int);
 int sleep(int);
 int uptime(void);
+int setpriority(int pid,int priority);
+int settimer(int,int);
 
 // ulib.c
 int stat(const char*, struct stat*);
