78e5e07cbf18d9568fa9bef47be46374158bd355
Harsha Rathi <harsharathi27@gmail.com>
2021-04-12 13:35:40 +0530

    Add field in a proc.h

--- proc.h
+++ proc.h
@@ -49,6 +49,9 @@ struct proc {
   struct file *ofile[NOFILE];  // Open files
   struct inode *cwd;           // Current directory
   char name[16];               // Process name (debugging)
+  int priority;	 // Priority of process
+  int timer;		 // Time to execute process
+  int totaltime;	 // Total time required to execute process
 };
 
 // Process memory is laid out contiguously, low addresses first:
