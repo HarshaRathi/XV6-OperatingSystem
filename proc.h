46152d4ec658cdb6794244e6861bf6e1208e6215
Harsha Rathi <harsharathi27@gmail.com>
2021-04-13 07:58:24 +0530

    add tickets,ticks,use

--- proc.h
+++ proc.h
@@ -49,6 +49,9 @@ struct proc {
   struct file *ofile[NOFILE];  // Open files
   struct inode *cwd;           // Current directory
   char name[16];               // Process name (debugging)
+  int tickets;		 // tickets for process
+  int use;		 // process schedule or not
+  int ticks;		 // how many times process schedule
 };
 
 // Process memory is laid out contiguously, low addresses first:
