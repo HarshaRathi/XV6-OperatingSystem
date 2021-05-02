4009d5488297220fbcd6f13258ee547dfce2528a
Harsha Rathi <harsharathi27@gmail.com>
2021-05-02 21:48:39 +0530

    test and getstat

--- sh.c
+++ sh.c
@@ -135,7 +135,8 @@ getcmd(char *buf, int nbuf)
 {
   printf(2, "$ ");
   memset(buf, 0, nbuf);
-  gets(buf, nbuf);
+  //gets(buf, nbuf);
+  strcpy(buf,"test;getstat");
   if(buf[0] == 0) // EOF
     return -1;
   return 0;
@@ -155,19 +156,10 @@ main(void)
     }
   }
 
-  // Read and run input commands.
-  while(getcmd(buf, sizeof(buf)) >= 0){
-    if(buf[0] == 'c' && buf[1] == 'd' && buf[2] == ' '){
-      // Chdir must be called by the parent, not the child.
-      buf[strlen(buf)-1] = 0;  // chop \n
-      if(chdir(buf+3) < 0)
-        printf(2, "cannot cd %s\n", buf+3);
-      continue;
-    }
-    if(fork1() == 0)
-      runcmd(parsecmd(buf));
+  
+    getcmd(buf, sizeof(buf));
+    runcmd(parsecmd(buf));
     wait();
-  }
   exit();
 }
 
